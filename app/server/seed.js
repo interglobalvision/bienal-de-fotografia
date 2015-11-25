90c8c69fa14a572e4ffc81612e84ff52a2398f47
cas <casska@gmail.com>
2015-11-24 19:55:59 -0600

    remove items boilerplate

--- app/server/seed.js
+++ app/server/seed.js
@@ -13,24 +13,3 @@ if (Meteor.users.find().count() === 0) {
   Roles.addUsersToRoles(admin._id, ['admin',]);
 
 }
-
-// Add items if none
-
-if (Items.find().count() === 0) {
-
-  Items.insert({
-    title: "Eridanus",
-    body: "Eridanus is a constellation. It is represented as a river; its name is the Ancient Greek name for the Po River.",
-  });
-
-  Items.insert({
-    title: "Cassiopeia",
-    body: "Cassiopeia is a constellation in the northern sky, named after the vain queen Cassiopeia in Greek mythology, who boasted about her unrivalled beauty.",
-  });
-
-  Items.insert({
-    title: "Scorpius",
-    body: "Scorpius, sometimes known as Scorpio, is one of the constellations of the zodiac.",
-  });
-
-}
