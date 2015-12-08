158ffe7209ef87559739163d3c435623af2b0be9
cas <casska@gmail.com>
2015-12-07 18:40:57 -0600

    add uploadImgae functions and slingshot config

--- app/client/templates/autoform/imageUpload.js
+++ app/client/templates/autoform/imageUpload.js
@@ -1,34 +1,35 @@
+var $hiddenInput,
+$fileInput,
+$thumbnail;
+
 Template.imageUpload.rendered = function () {
-  // TODO: Declare new Slingshot uploader
-  // Session.set('uploader', new Slingshot.Upload("imageUpload"));
 };
 
 Template.imageUpload.events({
   'change .image-upload-input': function(e) {
     e.preventDefault();
 
-    // TODO: Upload file
-    /*
-       $hiddenInput = $('#' + this.atts.id);
-       $loader = $hiddenInput.siblings('.preloader-wrapper');
-       $thumbnail = $hiddenInput.siblings('.image-upload-thumbnail');
+    var uploader = new Slingshot.Upload("imageUpload");
 
-       $loader.addClass('active');
+    $hiddenInput = $('#' + this.atts.id);
+    $loader = $hiddenInput.siblings('.preloader-wrapper');
+    $thumbnail = $hiddenInput.siblings('.image-upload-thumbnail');
 
-       uploader.send($hiddenInput.siblings('.image-upload-input')[0].files[0], function (error, downloadUrl) {
-       if (error) {
-       console.error('Error uploading', error.reason);
-       Materialize.toast(TAPi18n.__('alert-error'), 2000);
-       Materialize.toast(error.reason, 3000);
-       } else {
-       $hiddenInput.val(downloadUrl);
-       $thumbnail.attr('src', downloadUrl).show();
-       Materialize.toast(TAPi18n.__('alert-upload_success'), 3000);
-       }
+    $loader.addClass('active');
 
-       $loader.removeClass('active');
-       });
-       */
+    uploader.send($hiddenInput.siblings('.image-upload-input')[0].files[0], function (error, downloadUrl) {
+      if (error) {
+        console.log('Error uploading', error.reason);
+        Materialize.toast(TAPi18n.__('alert-error'), 2000);
+        Materialize.toast(error.reason, 3000);
+      } else {
+        $hiddenInput.val(downloadUrl);
+        $thumbnail.attr('src', downloadUrl).show();
+        Materialize.toast(TAPi18n.__('alert-upload_success'), 3000);
+      }
+
+      $loader.removeClass('active');
+    });
   },
 
   'click .image-upload-trigger': function(e) {
---
+++ app/lib/slingshot.js
@@ -0,0 +1,4 @@
+Slingshot.fileRestrictions("imageUpload", {
+  allowedFileTypes: ["image/png", "image/jpeg", "image/gif",],
+  maxSize: 4 * 1024 * 1024, // 4 MB
+});
