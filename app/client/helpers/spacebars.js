b7a66b28d37f8de99ef670586f7de8462f646d5c
mikey <michaelrayvon@gmail.com>
2016-01-05 15:04:35 -0600

    add isAdmin and isCommittee helpers

--- app/client/helpers/spacebars.js
+++ app/client/helpers/spacebars.js
@@ -9,3 +9,23 @@ Custom Handlebars helpers.
 Handlebars.registerHelper('myHelper', function(myArgument){
   return "Hello, " + myArgument;
 });
+
+Handlebars.registerHelper('isAdmin', function() {
+  var userId = Meteor.userId();
+
+  if (Roles.userIsInRole(userId, 'admin')) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+Handlebars.registerHelper('isCommittee', function() {
+  var userId = Meteor.userId();
+
+  if (Roles.userIsInRole(userId, 'committee')) {
+    return true;
+  } else {
+    return false;
+  }
+});
