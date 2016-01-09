909e9475038bc72cedbd5e245b545eebb972af75
mikey <michaelrayvon@gmail.com>
2016-01-08 18:08:24 -0600

    add create and remove user methods for admin

--- app/server/methods/userMethods.js
+++ app/server/methods/userMethods.js
@@ -13,4 +13,45 @@ Meteor.methods({
     Meteor.call('applicantEnrollmentEmail', userId);
 
   },
+
+  createUserRoles: function(userId, role) {
+    check(userId, String);
+
+    var result;
+
+    if ((role === 'committee' || role === 'admin') && Roles.userIsInRole(Meteor.userId(), ['admin',])) {
+      Roles.addUsersToRoles(userId, [role,]);
+      result = role;
+    } 
+
+    return result;
+  },
+
+  adminCreateUser: function(user) {
+//     check(userId, String);
+
+    var result;
+
+    if (Roles.userIsInRole(Meteor.userId(), ['admin',])) {
+      result = Accounts.createUser(user);
+    } else {
+      result = false;
+    }
+
+    return result;
+  },
+
+  removeUser: function(userId) {
+    check(userId, String);
+
+    var result;
+
+    if (Roles.userIsInRole(Meteor.userId(), ['admin',])) {
+      result = Meteor.users.remove({_id: userId,});
+    } else {
+      result = false;
+    }
+
+    return result;
+  },
 });
