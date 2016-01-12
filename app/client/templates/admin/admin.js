edf5c32234e1bda8dfa5860719460f098e24fa94
mikey <michaelrayvon@gmail.com>
2016-01-11 18:36:10 -0600

    use single user creation method for admin or committee roles

--- app/client/templates/admin/admin.js
+++ app/client/templates/admin/admin.js
@@ -15,26 +15,15 @@ Template.admin.events({
     event.preventDefault();
 
     var user = {
-      username: $('#username').val(),
-      email: $('#email').val(),
-    },
-    role = $('input[name="role"]:checked').val();
+        username: $('#username').val(),
+        email: $('#email').val(),
+      },
+      role = $('input[name="role"]:checked').val();
 
     if (!user.email || !role || !user.username) {
       Materialize.toast(TAPi18n.__('alert-please_fill'), 3000);
-    } else if (role === 'admin') {
-      Meteor.call('createAdminUser', user, function(error, result) {
-        if (error) {
-          Materialize.toast(TAPi18n.__('alert-error'), 2000);
-          Materialize.toast(error.reason, 3000);
-        } else {
-          $('#email').val('');
-          $('#username').val('');
-          $('input[name="role"]:checked').prop('checked', false);
-        }
-      });
-    } else if (role === 'committee') {
-      Meteor.call('createCommitteeUser', user, function(error, result) {
+    } else {
+      Meteor.call('adminCreateUser', user, role, function(error, result) {
         if (error) {
           Materialize.toast(TAPi18n.__('alert-error'), 2000);
           Materialize.toast(error.reason, 3000);
