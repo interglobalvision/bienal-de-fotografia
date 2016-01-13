3b83b36522f75cf381107eef8b1e449d570ec28a
mikey <michaelrayvon@gmail.com>
2016-01-12 19:05:13 -0600

    add ratings collection init

---
+++ app/collections/ratings.js
@@ -0,0 +1,6 @@
+/* ---------------------------------------------------- +/
+## Ratings ##
+All code related to the Ratings collection goes here.
+/+ ---------------------------------------------------- */
+
+Ratings = new Meteor.Collection('ratings');
