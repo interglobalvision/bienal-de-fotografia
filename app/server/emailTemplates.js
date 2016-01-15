a75200b58484910dd60e9d6c27ed4b9a0c17d3c3
cas <casska@gmail.com>
2016-01-15 17:23:13 -0600

    add enrollEmail template

--- app/server/emailTemplates.js
+++ app/server/emailTemplates.js
@@ -9,3 +9,12 @@ Accounts.emailTemplates.resetPassword.subject = function(user) {
 Accounts.emailTemplates.resetPassword.text = function (user, url) {
   return "Para restablecer tu contraseña ve al siguiente enlace: \n\n" + url;
 };
+
+// Enroll account
+Accounts.emailTemplates.enrollAccount.subject = function(user) {
+  return "Tu cuenta de jurado | XVII Bienal de Fotografía.";
+};
+Accounts.emailTemplates.enrollAccount.text = function(user, url) {
+  return "Hola. Puedes acceder a tu cuenta de jurado de la XVII Bienal de Fotografía en el siguiente link:\n\n"
+  + url;
+}
