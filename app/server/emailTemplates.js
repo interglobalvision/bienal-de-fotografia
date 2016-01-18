933233c2754de522fe85ecc980bb410a680e9745
mikey <michaelrayvon@gmail.com>
2016-01-18 16:57:32 -0600

    replace plain text in email templates with html close #64

--- app/server/emailTemplates.js
+++ app/server/emailTemplates.js
@@ -6,20 +6,19 @@ Accounts.emailTemplates.resetPassword.subject = function(user) {
  return "Tu nueva contraseña | XVII Bienal de Fotografía.";
 };
 
-Accounts.emailTemplates.resetPassword.text = function (user, url) {
-  return "Para restablecer tu contraseña ve al siguiente enlace: \n\n" + url;
+Accounts.emailTemplates.resetPassword.html = function (user, url) {
+  return "Para restablecer tu contraseña ve al siguiente enlace:<br><br><a href='" + url + "'>" + url + "</a>";
 };
 
 // Enroll account
 Accounts.emailTemplates.enrollAccount.subject = function(user) {
   return "Tu cuenta de jurado | XVII Bienal de Fotografía.";
 };
-Accounts.emailTemplates.enrollAccount.text = function(user, url) {
+Accounts.emailTemplates.enrollAccount.html = function(user, url) {
   if(Roles.userIsInRole(user,'committee')) {
     var role = "jurado";
   } else if (Roles.userIsInRole(user,'admin')) {
     var role = "administrador";
   }
-  return "Hola. Puedes acceder a tu cuenta de " + role + " de la XVII Bienal de Fotografía en el siguiente link:\n\n"
-  + url;
+  return "Hola. Puedes acceder a tu cuenta de <strong>" + role + "</strong> de la XVII Bienal de Fotografía en el siguiente link:<br><br><a href='" + url + "'>" + url + "</a>";
 }
