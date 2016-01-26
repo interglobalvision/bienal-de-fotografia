c49f8d6d54bdb926320c3201a904d372f8e933b2
cas <casska@gmail.com>
2016-01-25 22:29:05 -0600

    add cancel application markup adn event ahndler

--- app/client/templates/admin/submissions.js
+++ app/client/templates/admin/submissions.js
@@ -1,3 +1,28 @@
+Template.submissions.events({
+  'click .cancel-application': function(event){
+    event.preventDefault();
+
+    if(confirm(TAPi18n.__('admin.confirmCancelApplication'))) {
+
+      var applicationId = this._id;
+
+      if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
+        Meteor.call('cancelApplication', applicationId, function(error, result) {
+          if (error) {
+            alert(error);
+          } else {
+            Materialize.toast(TAPi18n.__('admin.applicationCanceled', 3000));
+          }
+        });
+      } else {
+        Materialize.toast("You don't have permission", 3000);
+        Router.go('/');
+      }
+
+    }
+  },
+});
+
 Template.submissions.helpers({
 
   ratingByApplication: function(applicationId) {
@@ -26,6 +51,15 @@ Template.submissions.helpers({
 
   applicationsCount: function() {
     return Applications.find().count();
+  },
+
+  applicationNotCanceled: function() {
+    var _this = this;
+
+    if(_this.status !== 'canceled') {
+      return true;
+    }
+    return false;
   },
 
 });
