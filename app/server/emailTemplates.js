901cb0d782511a56abaa1f856bc4b38834a0cf38
mikey <michaelrayvon@gmail.com>
2016-01-26 12:31:57 -0600

    fix email templates

--- app/server/emailTemplates.js
+++ app/server/emailTemplates.js
@@ -5,15 +5,25 @@ Accounts.emailTemplates.from = Meteor.settings.email_from;
 Accounts.emailTemplates.resetPassword.subject = function(user) {
  return "Tu nueva contraseña | XVII Bienal de Fotografía.";
 };
-
+Accounts.emailTemplates.resetPassword.text = function() {
+  return false;
+}
 Accounts.emailTemplates.resetPassword.html = function (user, url) {
   return "Para restablecer tu contraseña ve al siguiente enlace:<br><br><a href='" + url + "'>" + url + "</a>";
 };
 
 // Enroll account
 Accounts.emailTemplates.enrollAccount.subject = function(user) {
-  return "Tu cuenta de jurado | XVII Bienal de Fotografía.";
+  if(Roles.userIsInRole(user,'committee')) {
+    var role = "jurado";
+  } else if (Roles.userIsInRole(user,'admin')) {
+    var role = "administrador";
+  }
+  return "Tu cuenta de " + role + " | XVII Bienal de Fotografía.";
 };
+Accounts.emailTemplates.enrollAccount.text = function() {
+  return false;
+}
 Accounts.emailTemplates.enrollAccount.html = function(user, url) {
   if(Roles.userIsInRole(user,'committee')) {
     var role = "jurado";
